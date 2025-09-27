8e6c3f249f455c2503b246a8ab2fdf5b41112c41
jcmarreroc <j.c.marreroc@gmail.com>
2025-09-27 17:26:13 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='helmsman-flipbook-v33';
-const CORE=['./','./index.html','./content.js','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/updated_standards.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
